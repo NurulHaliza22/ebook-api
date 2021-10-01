05d761a86aa677f03c7b7ca0b423bc41b8a5ed8d
NurulHaliza22 <Rienzt2@gmail.com>
2021-10-01 20:54:15 +0700

    migration

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\BookController;
 
 Route::get('/me', [AuthController::class, 'me']);
+Route::resource('/books', BookController::class);
